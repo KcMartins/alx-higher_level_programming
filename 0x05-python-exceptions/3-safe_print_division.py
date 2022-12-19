06b59b6f0f1535797dbb51cb48acbeba8e5b0b36
KcMartins <kcmartinsikechukwu@yahoo.com>
2022-12-19 04:57:53 -0800

    Task 3

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,14 +1,10 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
-    """ Divide two integers and print the result
-    """
     try:
-        result = a / b
+        division = a / b
     except ZeroDivisionError:
-        result = None
+        division = None
     finally:
-        try:
-            print("Inside result: {}".format(result))
-        except NameError:
-            pass
-    return 
+        c = "Inside result:"
+        print("{} {}".format(c, division))
+        return division 
