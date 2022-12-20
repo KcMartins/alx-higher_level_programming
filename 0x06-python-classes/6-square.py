51b1821811a718f050f1469f026af84796e8b31c
KcMartins <kcmartinsikechukwu@yahoo.com>
2022-12-20 07:03:10 -0800

    Task 6 correction

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -1,56 +1,62 @@
 #!/usr/bin/python3
-class Square:
-    def __init__(self, size=0, position=(0, 0)):
-        self.size = size
-        self.position = position
+"""class Square that defines a square"""
 
-    # Size property
+
+class Square:
+    """class Square that defines a square"""
+
+    def __init__(self, size=0, position=(0, 0)):
+        """Instantiation with optional size and position"""
+        if not type(size) is int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        elif not type(position) is tuple or len(position) != 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        elif (not type(position[0]) is int) or (not type(position[1]) is int):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        elif position[0] < 0 or position[1] < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        else:
+            self.__size = size
+            self.__position = position
+
     @property
     def size(self):
+        """property to retrieve it"""
         return self.__size
 
-    # Size setter modifies
     @size.setter
     def size(self, value):
-        if type(value) != int:
-            raise TypeError('size must be an integer')
-        elif value < 0:
-            raise ValueError('size must be >= 0')
-        else:
-            self.__size = value
+        """property setter to set it"""
+        self.__size = value
 
-    # Position property
     @property
     def position(self):
+        """property to retrieve it"""
         return self.__position
 
-    # Position setter modifies
     @position.setter
     def position(self, value):
-        message = 'position must be a tuple of 2 positive integers'
-        if type(value) != tuple or len(value) != 2:
-            raise TypeError(message)
-
-        for items in value:
-            if type(items) != int or items < 0:
-                raise TypeError(message)
-
+        """property setter to set it"""
         self.__position = value
 
-    # Functions
     def area(self):
-        return self.__size ** 2
+        """Public instance that returns the  square area"""
+        return self.__size * self.__size
 
     def my_print(self):
-        size = self.__size
-        nl = self.__position[1]
-        ws = self.__position[0]
-
-        if size == 0:
-            print()
-
-        for newlines in range(nl):
-            print()
-
-        for row in range(size):
-            print((' ' * ws) + ('#' * size))
+        """Public instance that prints the square with the char #"""
+        if self.__size == 0:
+            print("")
+            return
+        if self.__position[1] > 0:
+            for l in range(0, self.__position[1]):
+                print("")
+        for i in range(0, self.__size):
+            if self.__position[0] > 0:
+                for k in range(0, self.__position[0]):
+                    print(" ", end="")
+            for j in range(0, self.__size):
+                print("#", end="")
+            print("")
